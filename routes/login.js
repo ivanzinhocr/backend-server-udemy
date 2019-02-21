16ea11f248de4b804ff36d34c672064cb4ca239e
Ivan Carballeira <icarba@yahoo.es>
2019-02-21 23:49:17 +0100

    Fin backend

--- routes/login.js
+++ routes/login.js
@@ -17,6 +17,22 @@ var CLIENT_ID = require('../config/config.js').CLIENT_ID;
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(CLIENT_ID);
 
+var mdAutenticacion = require('../middlewares/autenticacion');
+
+
+// =================================================
+// Autenticacion Google
+// =================================================
+app.get('/renuevatoken', mdAutenticacion.verificaToken, (req, res) => {
+
+    var token = jwt.sign({ usuario: req.usuario }, SEED, { expiresIn: 14400 }); // 4 horas
+
+    res.status(200).json({
+        ok: true,
+        token: token
+    });
+})
+
 
 // =================================================
 // Autenticacion Google
